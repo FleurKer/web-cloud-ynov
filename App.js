1b88d946b13f261fd14c0ae369282d70cf071b84
Fleur~ <fkernevez@outlook.fr>
2024-03-28 15:29:57 +0100

    tentative toaster + liaison avec gitHub

--- App.js
+++ App.js
@@ -1,31 +1,86 @@
 import React from "react";
-import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 import { signup } from "./auth_signup_password";
 import { signin } from "./auth_signin_password";
+import { RootSiblingParent } from "react-native-root-siblings";
+import Toast from "react-native-root-toast";
+import { signinWithGithub } from "./auth_github_signin_popup";
 
 export default function App() {
   const [email, onChangeEmail] = React.useState("");
   const [password, onChangePassword] = React.useState("");
 
+  //Regex for email validation
+  const emailRegex = /\S+@\S+\.\S+/;
+
+  //Regex for password validation
+  const passwordRegex =
+    /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+
+  const checkPassword = (password) => {
+    return passwordRegex.test(password);
+  };
+
+  const checkEmail = (email) => {
+    return emailRegex.test(email);
+  };
+
+  const testToast = () => {
+    Toast.show("Test works!");
+  };
+
+  handleSignup = (email, password) => {
+    if (checkEmail(email) && checkPassword(password)) {
+      signup(email, password);
+      console.log("Signup success");
+      // testToast();
+    } else {
+      console.log("Signup failed");
+      // testToast();
+    }
+  };
+
+  handleSignin = (email, password) => {
+    if (checkEmail(email) && checkPassword(password)) {
+      signin(email, password);
+      console.log("Signin success");
+      // testToast();
+    } else {
+      console.log("Signin failed");
+      // testToast();
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Email</Text>
-      <TextInput
-        style={styles.input}
-        onChangeText={onChangeEmail}
-        value={email}
-      ></TextInput>
-      <Text>Password</Text>
-      <TextInput
-        style={styles.input}
-        onChangeText={onChangePassword}
-        value={password}
-        secureTextEntry={true}
-      ></TextInput>
-      <Button title="Sign up" onPress={() => signup(email, password)}></Button>
-      <Button title="Sign in" onPress={() => signin(email, password)}></Button>
-    </View>
+    <RootSiblingParent>
+      <View style={styles.container}>
+        <Text>Email</Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={(text) => onChangeEmail(text)}
+          value={email}
+        ></TextInput>
+        <Text>Password</Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={(text) => onChangePassword(text)}
+          value={password}
+          secureTextEntry={true}
+        ></TextInput>
+        <Button
+          title="Sign up"
+          onPress={() => handleSignup(email, password)}
+        ></Button>
+        <Button
+          title="Sign in"
+          onPress={() => handleSignin(email, password)}
+        ></Button>
+        <Button
+          title="connect with github"
+          onPress={() => signinWithGithub()}
+        ></Button>
+      </View>
+    </RootSiblingParent>
   );
 }
 
